8b31746cdff977fca655595116ea40dddfba6920
Killerson <georg.laabe@ivkhk.ee>
2020-09-16 09:33:57 +0300

    Первая готовая версия

--- src/sptvr19/java/numberquessgame/App.java
+++ src/sptvr19/java/numberquessgame/App.java
@@ -11,9 +11,40 @@ class App {
         Scanner scan = new Scanner(System.in);
         Random rand = new Random();
         
-        //while(true) {
+        int max_number;
+        
+        System.out.print("\n\nInput the maximum: ");
+        max_number = scan.nextInt();
+
+        System.out.print("\nYou can not set the negative maximum! ");
+ 
+        int quessed_number = rand.nextInt(max_number);
+        System.out.println("\nNumber is quessed!");
+        System.out.println("---------------------");
+        
+        int your_number;
+        int attempts = 1;
+        
+        while(true) {
             
-        //}
+            System.out.print("\nInput your number: ");
+            your_number = scan.nextInt();
+            
+            if (your_number < quessed_number) {
+                System.out.println("Quessed number is bigger than inputed!");
+                attempts += 1;
+                continue;
+            } else if (your_number > quessed_number) {
+                System.out.println("Quessed number is smaller than inputed!");
+                attempts += 1;
+                continue;
+            } else {
+                System.out.println("\n ~~~ Congratulations! You guessed it! ~~~");
+                System.out.println("Attempts done: " + attempts);
+                break;
+            }
+            
+        }
     }
     
 }
