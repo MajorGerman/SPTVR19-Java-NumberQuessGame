f46a1ec1ececb017535be02b2cb4009eca7a2951
Killerson <georg.laabe@ivkhk.ee>
2020-09-16 09:02:42 +0300

    Заготовка

---
+++ src/sptvr19/java/numberquessgame/App.java
@@ -0,0 +1,19 @@
+package sptvr19.java.numberquessgame;
+import java.util.Random;
+import java.util.Scanner;
+        
+class App {
+    
+    public void run() {
+        System.out.println("||| Quess the Number! |||");
+        System.out.println(" ||| by Georg Laabe |||");
+        
+        Scanner scan = new Scanner(System.in);
+        Random rand = new Random();
+        
+        //while(true) {
+            
+        //}
+    }
+    
+}
---
+++ src/sptvr19/java/numberquessgame/SPTVR19JavaNumberQuessGame.java
@@ -0,0 +1,10 @@
+package sptvr19.java.numberquessgame;
+
+public class SPTVR19JavaNumberQuessGame {
+
+    public static void main(String[] args) {
+        App app = new App();
+        app.run();
+    }
+    
+}
