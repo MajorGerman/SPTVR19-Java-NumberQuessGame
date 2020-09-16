b7b26eab692c5e2232e78ab6891d1be1a45917a1
Killerson <georg.laabe@ivkhk.ee>
2020-09-16 09:58:18 +0300

    Добавлены попытки, минимум

--- src/sptvr19/java/numberquessgame/App.java
+++ src/sptvr19/java/numberquessgame/App.java
@@ -12,20 +12,25 @@ class App {
         Random rand = new Random();
         
         int max_number;
+        int min_number;
         
-        System.out.print("\n\nInput the maximum: ");
+        System.out.print("\n\nInput the minimum: ");
+        min_number = scan.nextInt();    
+        
+        System.out.print("Input the maximum: ");
         max_number = scan.nextInt();
-
-        System.out.print("\nYou can not set the negative maximum! ");
+        
+        System.out.print("\nInput the amount of tries: ");
+        int tries = scan.nextInt();
  
-        int quessed_number = rand.nextInt(max_number);
+        int quessed_number = rand.nextInt(max_number-min_number+1)+min_number;
         System.out.println("\nNumber is quessed!");
         System.out.println("---------------------");
         
         int your_number;
         int attempts = 1;
         
-        while(true) {
+        do {
             
             System.out.print("\nInput your number: ");
             your_number = scan.nextInt();
@@ -33,18 +38,21 @@ class App {
             if (your_number < quessed_number) {
                 System.out.println("Quessed number is bigger than inputed!");
                 attempts += 1;
-                continue;
+                tries -= 1;
             } else if (your_number > quessed_number) {
                 System.out.println("Quessed number is smaller than inputed!");
                 attempts += 1;
-                continue;
+                tries -= 1;
             } else {
                 System.out.println("\n ~~~ Congratulations! You guessed it! ~~~");
-                System.out.println("Attempts done: " + attempts);
+                System.out.println(" ~~~ Attempts done: " + attempts + " ~~~");
                 break;
             }
             
+        } while (tries > 0);
+        
+        if (tries <= 0) {
+            System.out.println("\n ~~~ You lose! The quessed number was: " + quessed_number + " ~~~");
         }
     }
-    
 }
